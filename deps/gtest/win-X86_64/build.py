4d16143370bb59f595659083026fd1f44e2e58a4
Nicolas SURBAYROLE <nsurbayrole@quarkslab.com>
2019-04-05 13:05:23 +0200

    VS 2019
    Use flag "-DCMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING"
    to compile gtest 1.7.0 and QBDI

--- deps/gtest/win-X86_64/build.py
+++ deps/gtest/win-X86_64/build.py
@@ -37,8 +37,9 @@ elif sys.argv[1] == "build":
         shutil.rmtree("build")
     os.mkdir("build")
     subprocess.check_call(["cmake", "../googletest-release-" + VERSION,
-                           "-G", "Visual Studio 14 2015 Win64",
+                           "-G", "Visual Studio 16 2019",
                            "-DCMAKE_BUILD_TYPE=Release",
+                           "-DCMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING",
                            "-Dgtest_force_shared_crt=On"], cwd="build")
     subprocess.check_call(["MSBuild.exe", "ALL_BUILD.vcxproj",
                            "/p:Configuration=Release,Platform=X64"],
